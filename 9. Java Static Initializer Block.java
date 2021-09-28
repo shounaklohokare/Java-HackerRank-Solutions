a9e2e23c64d8d01e0bddc0fe97259ced10bd34b1
Shounak Lohokare <41364885+shounaklohokare@users.noreply.github.com>
2021-09-28 17:13:36 +0530

    Create 9. Java Static Initializer Block.java

---
+++ 9. Java Static Initializer Block.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+static boolean flag=false;
+static int H;
+static int B;
+static{
+    
+    flag=false;
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    B = scanner.nextInt();
+    H = scanner.nextInt();
+    
+    if(B>0 && H>0){
+        flag=true;
+    }else{
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+    
+    scanner.close();
+    
+    
+    
+}
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
